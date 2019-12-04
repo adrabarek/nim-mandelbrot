0b17b1b468150905c21216d0923f5a0044fe7796
Andrzej Drabarek <andrzej.drabarek@protonmail.com>
2019-12-04 21:45:06 +0100

    Cleanup.

--- mandelbrot.nim
+++ mandelbrot.nim
@@ -1,23 +1,10 @@
 import math
 import complex
 
-const 
-  screen_width = 100
-  screen_height = 50
-  max_iters = 100
-  cutoff = 1.0e2
-
 proc f(z: Complex64, c: Complex64): Complex64 =
   return pow(z, 2.0) + c
 
-proc test_pixel(x: int, y: int): bool =
-  const 
-    sw = float(screen_width)
-    sh = float(screen_height)
-  let
-    r = 4.0*(float(x)/sw - 0.5f)
-    i = 4.0*(float(y)/sh - 0.5f)
-    c = complex64(r, i)
+proc mandelbrot_test(c: Complex64, max_iters: int, cutoff: float): bool =
   var 
     z = complex64(0.0, 0.0)
 
@@ -27,9 +14,21 @@ proc test_pixel(x: int, y: int): bool =
       return false
   return true
 
+const 
+  screen_width = 100
+  screen_height = 50
+  screen_width_f = float(screen_width)
+  screen_height_f = float(screen_height)
+  max_iters = 100
+  cutoff = 1.0e2  
+
 for y in countDown(screen_height - 1, 0):
   for x in 0..<screen_width:
-    if test_pixel(x, y):
+    var
+      r = 4.0*(float(x)/screen_width_f - 0.5f)
+      i = 4.0*(float(y)/screen_height_f - 0.5f)
+      c = complex64(r, i)
+    if mandelbrot_test(c, max_iters, cutoff):
       write(stdout, '@')
     else:
       write(stdout, '.')
